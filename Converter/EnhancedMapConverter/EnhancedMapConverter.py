2ce702b8e66be6c6396b5bcb2c5059ef4021d535
giga487 <giga487@hotmail.com>
2021-12-30 02:19:28 +0100

    avanzamenti folli

--- Converter/EnhancedMapConverter/EnhancedMapConverter.py
+++ Converter/EnhancedMapConverter/EnhancedMapConverter.py
@@ -4,21 +4,58 @@ import os.path
 import ntpath
 
 class StringRead:
+    DEFAULT_ZOOM_LEVEL = 0
+    DEFAULT_COLOR = "white"
 
-    def __init__(self, _stringToSplit: str):
+    def __init__(self, _stringToSplit: str, _file):
 
         self.m_stringToSplit = _stringToSplit
+        self.m_pathfile = _file
+        self.EncapsuleData()
 
+    def GetString(self):
+        return self.m_stringToSave
 
     def EncapsuleData(self):
         self.m_splittedString = self.m_stringToSplit.split()
 
-        self.m_Name = self.m_splittedString[1] + self.m_splittedString[2]
-        self.m_X = self.m_splittedString[3]
-        self.m_Y = self.m_splittedString[4]
-        self.m_mapIndex = self.m_splittedString[5]
-        self.NameInMap = self.m_splittedString[6]
+        self.m_Name = ""
+
+        IsNotCoordinate = True
+        #starto da dopo il +
+        i = 1
+
+        try:
+
+            while IsNotCoordinate == True:
+
+                try:
+                    intValueToCheck = int(self.m_splittedString[i])
+                    IsNotCoordinate = False
+                    break
+                except:
+                    self.m_Name += " " + self.m_splittedString[i]
+                i += 1
+
+            self.m_X = self.m_splittedString[i]
+            i+=1
+            self.m_Y = self.m_splittedString[i]
+            i+=1
+            self.m_mapIndex = self.m_splittedString[i]
+            i+=1
+            self.NameInMap = self.m_splittedString[i]
+
+            self.m_stringToSave = f"{self.m_X},{self.m_Y},{self.m_mapIndex},{self.m_Name},{self.DEFAULT_COLOR},{self.DEFAULT_ZOOM_LEVEL}"
+            print(self.m_stringToSave)
+
+        except:
+            print(f"Error on parse string \"{self.m_stringToSplit}\" in file {self.m_pathfile}")
            
+class FileWriter:
+
+    def __init__(self, _pathFiles: str, ):
+        print("BOH")
+
 
 
 class FileToRead:
@@ -26,23 +63,57 @@ class FileToRead:
     def __init__(self, path: str):
         self.m_pathfile = path
 
+        self.LoadFiles()
+
+
     def FindNameGroup(self):
         head, tail = ntpath.split(self.m_pathfile)
         return tail or ntpath.basename(head)
 
+    def LoadFiles(self):
+
+        self.m_listLine = []
+
+        try:
+            with open(self.m_pathfile) as f:
+                lines = f.readlines()
+
+            if(lines != []):
+                print(f"\n+++++++++++ Read file {self.FindNameGroup()} ++++++++++++")
+                for line in lines:
+                    self.m_listLine.append(StringRead(line, self.m_pathfile))
+        except:
+            print("Error")
+
+    def GetString(self):
+
+        m_list = []
+        for line in self.m_listLine:
+            m_list.append(line.GetString())
+    
+        return m_list
+
 
 applicationPath = os.getcwd()
-folderToAnalyze = "Definition"
+folderToAnalyze = "Definitions"
 
 def main():
+    print("Welcome to the enhanced map Converter by Giga487")
+    pathToAnalyze = os.path.join(applicationPath, folderToAnalyze)
+    print(f"Analyze the path {pathToAnalyze}")
+    fileList = os.listdir(pathToAnalyze)
 
-    fileList = os.listdir(os.path.join(applicationPath,folderToAnalyze))
+    listFile = []
+    textOnFileToWrite = []
 
     for fileToAnal in fileList:
 
-        FileToRead(fileToAnal)
+        listFile.append(FileToRead(os.path.join(pathToAnalyze, fileToAnal)))
 
+    for listFileToWrite in listFile:
+        textOnFileToWrite.append(listFileToWrite.GetString())
 
+    text = input("")
 
 
 if __name__ == "__main__":
